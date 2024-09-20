e8ac33794eba842676c9a954c349e45ffc4d674c
EkaNurulFalah <ekanurulfalah66@gmail.com>
2024-09-20 14:19:03 +0700

    Tugas bu Rizza Jurnal_02

---
+++ main.java
@@ -0,0 +1,7 @@
+class main {
+    public static void main(String [] args) {
+        System.out.println("Nama saya : Eka Nurul Falah");
+        System.out.println("NIM : 60706240018");
+        System.out.println("Kelas D3IF - 48 - 01");
+    }
+}
